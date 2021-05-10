23820e004293ebefc446fc3790344038a9af723b
raj-k122 <70319976+raj-k122@users.noreply.github.com>
2021-05-10 21:39:53 +1000

    Update webBot.py

--- webBot.py
+++ webBot.py
@@ -1,15 +1,5 @@
 from selenium import webdriver
 
-#The addressify website does not allow any user to enter in
-# an address into the site. To use addressify you must create an API call. I don't know if
-# you knew this or not or maybe I misunderstood the question. But it was clear that i could not
-# simply enter an address into the search bar of addressify.
-
-# So as an alternative, to show you that I have the skills to make an automated webBot system,
-# I used the official python website as a replacement and created a function that automatically
-# takes the user's query and prints the results of the page that follow.
-
-
 def automated(query):
     #define the location with your chrome driver is installed
     driver = webdriver.Chrome(executable_path=r'C:\Users\rajku\Desktop\chromedriver.exe')
